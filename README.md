4ae153e50bf1fe9b503b3693e59f4583d4a3589a
goo-gy <lkukh17@naver.com>
2022-04-08 23:32:15 +0900

    func : signUp with jwt
    - signUp에서 token 발행
    - token으로 user 조회시 본인 정보 조회
    - env에서 secret key 설정

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # letters-graphQL
+
 The back-end project of Letters
+
+## JWT Scret Key 설정
+
+```
+SECRET_KEY=[SECRET KEY]
+```
--- graphql/resolver.js
+++ graphql/resolver.js
@@ -2,7 +2,7 @@ import { user, users, signIn, signUp } from './user';
 
 const resolvers = {
   Query: {
-    user: (_, { id }) => user({ id }),
+    user: (_, { id, token }) => user({ id, token }),
     users: () => users(),
     signIn: (_, { email, password }) => signIn({ email, password }),
   },
